71837a6492d7986fbf5eca3f652f13b450a147fe
Aliya Nigamova <aliya.nigamova@cern.ch>
2025-01-17 16:31:16 +0100

    split v123 and v4 datasets in 2023 because jme implemented correcttions separately https://gitlab.cern.ch/cms-nanoAOD/jsonpog-integration/-/tree/master/POG/JME?ref_type=heads

--- cmsdb/campaigns/run3_2023_preBPix_nano_tau_skim_v2/data.py
+++ cmsdb/campaigns/run3_2023_preBPix_nano_tau_skim_v2/data.py
@@ -12,42 +12,90 @@ CMS datasets from the 2023 preBPix data-taking campaign /eos/cms/store/group/phy
 """
 
 cpn.add_dataset(
-    name='data_e_C',
+    name='data_e_Cv123',
     id=2212010,
     is_data=True,
     processes=[procs.data_e],
-    keys=['/EGamma0_Run2023C_v1', '/EGamma0_Run2023C_v2', '/EGamma0_Run2023C_v3', '/EGamma0_Run2023C_v4','/EGamma1_Run2023C_v1', '/EGamma1_Run2023C_v2', '/EGamma1_Run2023C_v3', '/EGamma1_Run2023C_v4'],
-    n_files=38 + 11 + 13 + 94 + 38 + 11 + 13 + 94,
-    n_events=30038670 + 7890231 + 9817005 + 72118251 + 30010181 + 7891642 + 9818391 + 72080022,
+    keys=['/EGamma0_Run2023C_v1', '/EGamma0_Run2023C_v2', '/EGamma0_Run2023C_v3', '/EGamma1_Run2023C_v1', '/EGamma1_Run2023C_v2', '/EGamma1_Run2023C_v3'],
+    n_files=38 + 11 + 13 + 38 + 11 + 13,
+    n_events=30038670 + 7890231 + 9817005 + 30010181 + 7891642 + 9818391,
     aux={
-        'era': 'C'
+        'era': 'C',
+        'jec_era': 'Cv123'
     }
 )
 
+cpn.add_dataset(
+    name='data_e_Cv4',
+    id=221201001,
+    is_data=True,
+    processes=[procs.data_e],
+    keys=['/EGamma0_Run2023C_v4', '/EGamma1_Run2023C_v4'],
+    n_files=94 + 94,
+    n_events= 72118251 + 72080022,
+    aux={
+        'era': 'C',
+        'jec_era': 'Cv4'
+    }
+)
 
 cpn.add_dataset(
-    name='data_mu_C',
+    name='data_mu_Cv123',
     id=2212011,
     is_data=True,
     processes=[procs.data_mu],
-    keys=['/Muon0_Run2023C_v1', '/Muon0_Run2023C_v2', '/Muon0_Run2023C_v3', '/Muon0_Run2023C_v4','/Muon1_Run2023C_v1', '/Muon1_Run2023C_v2', '/Muon1_Run2023C_v3', '/Muon1_Run2023C_v4'],
-    n_files=27 + 9 + 11 + 75 + 27 + 9 + 11 + 75,
-    n_events= 25754131 + 8049224 + 9788396 + 71162252 + 25717768 + 8050781 + 9787601 + 71111717,
+    keys=['/Muon0_Run2023C_v1', '/Muon0_Run2023C_v2', '/Muon0_Run2023C_v3', '/Muon1_Run2023C_v1', '/Muon1_Run2023C_v2', '/Muon1_Run2023C_v3'],
+    n_files=27 + 9 + 11 + 27 + 9 + 11,
+    n_events= 25754131 + 8049224 + 9788396 + 25717768 + 8050781 + 9787601 ,
     aux={
-        'era': 'C'
+        'era': 'C',
+        'jec_era': 'Cv123'
     }
 )
 
+
 cpn.add_dataset(
-    name='data_tau_C',
+    name='data_mu_Cv4',
+    id=221201101,
+    is_data=True,
+    processes=[procs.data_mu],
+    keys=['/Muon0_Run2023C_v4', '/Muon1_Run2023C_v4'],
+    n_files= 75 + 75,
+    n_events= 71162252 + 71111717,
+    aux={
+        'era': 'C',
+        'jec_era': 'Cv4'
+    }
+)
+
+
+cpn.add_dataset(
+    name='data_tau_Cv123',
     id=2212012,
     is_data=True,
     processes=[procs.data_tau],
-    keys=['/Tau_Run2023C_v1', '/Tau_Run2023C_v2', '/Tau_Run2023C_v3', '/Tau_Run2023C_v4'],
-    n_files=15 + 5 + 7 + 45,
-    n_events=10255817 + 3607056 + 4585815 + 32314548,
+    keys=['/Tau_Run2023C_v1', '/Tau_Run2023C_v2', '/Tau_Run2023C_v3'],
+    n_files=15 + 5 + 7,
+    n_events=10255817 + 3607056 + 4585815,
     aux={
         'era': 'C',
+        'jec_era': 'Cv123'
+    }
+)
+
+
+
+cpn.add_dataset(
+    name='data_tau_Cv4',
+    id=221201201,
+    is_data=True,
+    processes=[procs.data_tau],
+    keys=['/Tau_Run2023C_v4'],
+    n_files= 45,
+    n_events= 32314548,
+    aux={
+        'era': 'C',
+        'jec_era': 'Cv4'
     }
 )
 
