04c54db28d207d73a3a7e3d1e699e44f23a14acc
Aliya Nigamova <anigamov@cern.ch>
2025-05-07 10:49:59 +0200

    add signal sample from PR#10

--- cmsdb/campaigns/run3_2022_postEE_v2_nano_tau_v14/__init__.py
+++ cmsdb/campaigns/run3_2022_postEE_v2_nano_tau_v14/__init__.py
@@ -25,3 +25,4 @@ import cmsdb.campaigns.run3_2022_postEE_v2_nano_tau_v14.ewk
 import cmsdb.campaigns.run3_2022_postEE_v2_nano_tau_v14.data
 import cmsdb.campaigns.run3_2022_postEE_v2_nano_tau_v14.top
 import cmsdb.campaigns.run3_2022_postEE_v2_nano_tau_v14.signal
+import cmsdb.campaigns.run3_2022_postEE_v2_nano_tau_v14.higgs
--- cmsdb/campaigns/run3_2022_preEE_nano_tau_skim_v2/__init__.py
+++ cmsdb/campaigns/run3_2022_preEE_nano_tau_skim_v2/__init__.py
@@ -26,4 +26,5 @@ import cmsdb.campaigns.run3_2022_preEE_nano_tau_skim_v2.ewk
 import cmsdb.campaigns.run3_2022_preEE_nano_tau_skim_v2.data
 import cmsdb.campaigns.run3_2022_preEE_nano_tau_skim_v2.top
 import cmsdb.campaigns.run3_2022_preEE_nano_tau_skim_v2.signal
+import cmsdb.campaigns.run3_2022_preEE_nano_tau_skim_v2.higgs
 
